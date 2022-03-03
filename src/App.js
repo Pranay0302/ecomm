a4380e9f8efb022e8168764c28a12af8e228f919
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2022-03-03 16:57:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <h4>STORE LISTING ASSIGNMENT</h4>
+          <small>assignment given by treflo</small>
+    </div>
+  );
+}
+
+export default App;
